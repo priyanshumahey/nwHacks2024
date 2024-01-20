b185a178ffa41db1e1658be14617dc11e5c4ca75
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-01-20 15:30:07 -0800

    Schema update

--- nwhacks/lib/db/schema.ts
+++ nwhacks/lib/db/schema.ts
@@ -1,15 +1,24 @@
 import { mysqlTable, serial, text, varchar, int } from 'drizzle-orm/mysql-core'
-
+import { relations } from "drizzle-orm";
 
 export const user = mysqlTable('user', {
   id: serial('id').primaryKey(),
   fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
 })
 
-export const post = mysqlTable('post', {
+export const inviteList = mysqlTable('invite_list', {
+  id: serial('id').primaryKey(),
+  eventId: int('event_id'),
+  userId: int('user_id'),
+});
+
+export const event = mysqlTable('event', {
   id: serial('id').primaryKey(),
   title: text('title'),
-  likes: int('likes'),
-  userId: int('userId'),
-})
+  description: text('description'),
+  location: text('location'),
+  startTime: text('start_time'),
+  endTime: text('end_time'),
+  creatorId: int('creator_id'),
+  inviteList: int('invite_list')//.references(() => inviteList.id)
+});
