0fbe3f23fbad9220a130e315a6c77da2c83c7e91
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-01-20 12:49:57 -0800

    Set up drizzle config properly

--- nwhacks/lib/db/schema.ts
+++ nwhacks/lib/db/schema.ts
@@ -1,13 +1,15 @@
-import {
-    mysqlTable,
-    serial,
-    timestamp,
-    varchar,
-} from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, text, varchar, int } from 'drizzle-orm/mysql-core'
 
-export const users = mysqlTable('users', {
-    id: serial('id').primaryKey().notNull(),
-    name: varchar('name', { length: 256 }),
-    email: varchar('email', { length: 256 }),
-    createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow(),
-});
+
+export const user = mysqlTable('user', {
+  id: serial('id').primaryKey(),
+  fullName: text('full_name'),
+  phone: varchar('phone', { length: 256 }),
+})
+
+export const post = mysqlTable('post', {
+  id: serial('id').primaryKey(),
+  title: text('title'),
+  likes: int('likes'),
+  userId: int('userId'),
+})
