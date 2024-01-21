7c852c2cbdfb34a1ea2130bd100d90a2f315375f
TL0226 <TL0226yn@gmail.com>
2024-01-21 02:37:40 -0800

    can add users to database

--- nwhacks/app/calendar/page.tsx
+++ nwhacks/app/calendar/page.tsx
@@ -4,13 +4,56 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import React, {useState} from 'react'
+import Popup from 'reactjs-popup';
+import 'bulma/css/bulma.min.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import styles from './styles.module.css';
+import Button from '@mui/material/Button';
+import {calendarAdd} from './calendarController';
 
 export default function CalendarPage() {
 
-    const [events, setEvents] = useState([{title: 'nice event', description: '', location: 'Vancouver', start: new Date(), end: new Date(), creatorId: 'a', inviteList: ''}])
-    const addEvent = (event : {title: string, description: string, location:string, start: Date, end: Date, creatorId: string, inviteList: string}) => {
-        setEvents([...events, event])
+  //modal popup and events
+    const [modal, setModal] = useState(false);
+    const [events, setEvents] = useState([{title: 'nice event', description: '', location: 'Vancouver', start: new Date(), end: new Date(), creatorId: 1, inviteList: 0}]);
+    const [popupOpen, setPopupOpen] = useState(false);
+
+    //form fields
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [location, setLocation] = useState<string>('');
+    const [startDate, setStartDate] = useState<Date | null>(null);
+    const [endDate, setEndDate] = useState<Date | null>(null);
+    const [inviteList, setInviteList] = useState<number>(0);
+
+    const addEvent = () => {
+        console.log('hello');
+        if (typeof(endDate) != null && typeof(startDate) != null) {
+          setEvents([...events, {title: title, description: description, location: location, start: startDate, end: endDate, creatorId: 0, inviteList: inviteList}])
+          calendarAdd({id: (new Date().getTime()), title: title, description: description, location: location,
+            startTime: startDate.toString(), endTime: endDate.toString(), creatorId: 0, inviteList: inviteList});
+        }
+        else {
+          console.log('please specify date');
+        }
     }
+
+    const openModal = () => {
+      setPopupOpen(true);
+  };
+
+  const closeModal = () => {
+      setPopupOpen(false);
+  };
+
+    //styles for modal
+    const style = { background: 'rgba(142 79 79 1)' };
+
   return (
       <div className='calendar-container'>
         <FullCalendar
@@ -45,17 +88,7 @@ export default function CalendarPage() {
               addEvent: {
                 text: 'add event',
                 click: () => {
-                    let dateStr = prompt('Enter a date in YYYY-MM-DD format');
-                    let startDate = new Date(dateStr + 'T00:00:00'); // will be in local time
-                    let endDate = new Date(startDate);
-                    endDate.setDate(endDate.getTime() + 3600);
-
-                if (!isNaN(startDate.valueOf())) { // valid?
-                    addEvent({title: 'some random event', description: '', location: 'Vancouver', start: startDate, end: endDate, creatorId: 'a', inviteList: ''})
-                    alert('Great. Now, update your database...');
-                } else {
-                    alert('Invalid date.');
-                }
+                    openModal();
                 }
               }
           }}
@@ -65,7 +98,61 @@ export default function CalendarPage() {
           selectable={true}
           selectMirror={true}
           events={events}
+          dateClick={openModal}
         />
+           <Popup
+                open={popupOpen}
+                onClose={closeModal}
+                modal
+                nested
+            >
+                <div className={styles.popup}>
+                    <Typography variant='h3'>Add new event</Typography>
+                      <Box component='form' noValidate autoComplete='off'>
+                        <TextField
+                        className={styles.formfield}
+                          color="success"
+                          label='title'
+                          value={title}
+                          onChange={(e: any) => {
+                          return setTitle(e.target.value);
+                          }}
+                        />
+                        <TextField
+                        className={styles.formfield}
+                          color="success"
+                          label='description'
+                          value={description}
+                          onChange={(e: any) => {
+                          return setDescription(e.target.value);
+                          }}
+                        />
+                        <TextField
+                        className={styles.formfield}
+                          color="success"
+                          label='location'
+                          value={location}
+                          onChange={(e: any) => {
+                          return setLocation(e.target.value);
+                          }}
+                        />
+                      <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker className={styles.formfield} label='start time' onChange={(date) => setStartDate(date)} value={startDate} />
+                        <DateTimePicker className={styles.formfield} label='end time' onChange={(date) => setEndDate(date)} value={endDate} />
+                      </LocalizationProvider>
+                      <TextField
+                      className={styles.formfield}
+                          color="success"
+                          label='invite list'
+                          value={inviteList}
+                          onChange={(e: any) => {
+                          return setInviteList(e.target.value);
+                          }}
+                        />
+                        <Button onClick={() => {addEvent()}} variant="contained">Submit</Button>
+                    </Box>
+                </div>
+            </Popup>;
       </div>
   )
 }
